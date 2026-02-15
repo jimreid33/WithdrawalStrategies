0ff13af554adb9c59a2ed9b0e551def47c006aa5
jimreid33 <jimreid33@gmail.com>
2026-02-14 19:33:48 -0800

    swap tailwindcss packages

--- vite-config.js
+++ vite-config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react({ jsxRuntime: 'automatic' }),
-    tailwindcss()
-  ],
-  css: {
-    postcss: false, // This tells Vite: "DO NOT use PostCSS at all"
-  },
-  base: '/',
+  plugins: [react()],
 })
